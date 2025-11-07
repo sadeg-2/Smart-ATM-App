afcb4610f8cc81b1dabda471f1a4b176de4a5a55
sadeg ashour <sadeg.magde@outlook.sa>
2025-11-07 16:30:51 +0200

    update work

--- src/Pages/history.tsx
+++ src/Pages/history.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuthStore } from '../Context/authSContext';
 import {
-  ArrowLeft,
   Filter,
-  Download,
   Search,
   Calendar,
   TrendingUp,
