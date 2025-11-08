5c3d7bb475b15aabb51f3b26d1bcea4519d299ee
nada khairaldin <nadakhairaldin8@gmail.com>
2025-11-08 20:30:01 +0200

    add a loader during login

--- src/Pages/login.tsx
+++ src/Pages/login.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../Context/authSContext";
 import "../Styles/Login.scss"; // SCSS file with glassmorphism styles
 import type { UserInfo } from "../Types/types";
-import Loader from "../Component/Loader";
 
 export default function Login() {
   const { login } = useAuthStore();
@@ -15,16 +14,15 @@ export default function Login() {
 
   const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setIsLoading(true); 
 
     try {
+      setIsLoading(true);
       const res = await fetch(
         "https://69060c47ee3d0d14c134982d.mockapi.io/users"
       );
       const users: UserInfo[] = await res.json();
-
       const user = users.find(
-        (u) => u.user_name === userName && u.pin === pin
+        (user) => user.user_name === userName && user.pin === pin
       );
 
       if (!user) {
@@ -43,40 +41,48 @@ export default function Login() {
 
   return (
     <div className="login-page">
-      {isLoading ? (
-          <Loader />
-      ) : (
-        <>
-          <div className="background">
-            <div className="shape"></div>
-            <div className="shape"></div>
+      <div className="background">
+        <div className="shape"></div>
+        <div className="shape"></div>
+      </div>
+
+      <form className="form" autoComplete="off" onSubmit={handleLogin}>
+        <h3>Sign In</h3>
+
+        <div className="control block-cube block-input">
+          <input
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            placeholder="Username"
+          />
+          <div className="bg-top">
+            <div className="bg-inner"></div>
           </div>
+          <div className="bg-right">
+            <div className="bg-inner"></div>
+          </div>
+          <div className="bg">
+            <div className="bg-inner"></div>
+          </div>
+        </div>
 
-          <form className="form" autoComplete="off" onSubmit={handleLogin}>
-            <h3>Sign In</h3>
-
-            <div className="control block-cube block-input">
-              <input
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                placeholder="Username"
-              />
-              <div className="bg-top"><div className="bg-inner"></div></div>
-              <div className="bg-right"><div className="bg-inner"></div></div>
-              <div className="bg"><div className="bg-inner"></div></div>
-            </div>
-
-            <div className="control block-cube block-input">
-              <input
-                type="password"
-                value={pin}
-                onChange={(e) => setPin(e.target.value)}
-                placeholder="PIN"
-              />
-              <div className="bg-top"><div className="bg-inner"></div></div>
-              <div className="bg-right"><div className="bg-inner"></div></div>
-              <div className="bg"><div className="bg-inner"></div></div>
-            </div>
+        <div className="control block-cube block-input">
+          <input
+            type="password"
+            value={pin}
+            onChange={(e) => setPin(e.target.value)}
+            placeholder="PIN"
+          />
+          <div className="bg-top">
+            <div className="bg-inner"></div>
+          </div>
+          <div className="bg-right">
+            <div className="bg-inner"></div>
+          </div>
+          <div className="bg">
+            <div className="bg-inner"></div>
+          </div>
+        </div>
 
         <button
           type="submit"
@@ -87,11 +93,6 @@ export default function Login() {
           <span className="text">Log In</span>
         </button>
       </form>
-      </>
-      )}
     </div>
-
   );
 }
-
-
